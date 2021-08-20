539e1c3cbaa44e047a91c588fee0fe79340225c5
James <jamiem842@gmail.com>
2021-08-20 19:00:10 +0100

    Misc script removals

--- src/claimExtraction/scripts/download-model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p "../models"
-gsutil cp "gs://comp66090-storage/claimExtraction/finetuned-models/roberta-large (large-manual) 71.zip" "../models/roberta-large (large-manual) 71.zip"
-unzip "../models/roberta-large (large-manual) 71.zip" -d "../models/roberta-large (large-manual) 71"
-rm -f "../models/roberta-large (large-manual) 71.zip"
